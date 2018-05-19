aa44a4febac99364c4a8c08986fb4e0531475a87
Nathaniel Jansen <nathanieljansen@gmail.com>
2018-05-18 18:55:30 -0600

    the basics are there

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({
+  defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+app.get("/", (req, res) => {
+    res.render("index");
+  });
+  app.get("/contact", (req, res) => {
+    res.render("contact");
+  });
+   app.get("/about", (req, res) => {
+     res.render("about");
+   });
+
+app.listen(PORT, () => {
+  console.log("App listening on PORT " + PORT);
+});
