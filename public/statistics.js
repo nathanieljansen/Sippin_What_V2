6a68e194ba31ebad1302f691cf0063e6223968e4
Nathaniel Jansen <nathanieljansen@gmail.com>
2018-05-30 17:42:42 -0600

    fixes for statistics

--- public/statistics.js
+++ public/statistics.js
@@ -2,57 +2,59 @@ $(document).ready(function () {
     console.log("Stat page is up!");
     $.ajax({
         method: "GET",
-        url: "/api/zip",
-    }).then(function (dataFromDb) {
-        console.log('you get all the wines finally', dataFromDb)
-        var allZipCodes = dataFromDb.map(foodpairings => foodpairings.zip);
-        console.log('Zip Code:', allZipCodes)
-        var chart = c3.generate({
-            bindto: '#chartZip',
-            data: {
-                columns: [
-                    ["Count", count],
-                    ["Zip", allZipCodes],
-                ],
-                type: 'bar',
-                //type: 'donut',
-                // title: 'Zip Code',
-    
-                onmouseover: function (d, i) {
-                    console.log("onmouseover", d, i, this);
-                },
-                onmouseout: function (d, i) {
-                    console.log("onmouseout", d, i, this);
-                },
-                onclick: function (d, i) {
-                    console.log("onclick", d, i, this);
-                },
-            },
-            axis: {
-                x: {
-                    label: 'Zip Code'
-                },
-                y: {
-                    label: 'Count'
-                }
-            }
-    
-        });
-        // setTimeout(function () {
-        //     chart.load({
-        //         columns: [
-        //             ["wine", 130],
-        //         ]
-        //     });
-        // }, 1000);
-    
-        // setTimeout(function () {
-        //     chart.unload({
-        //         ids: 'Merlot'
-        //     });
-        // }, 2000);    
-    });
-});
+        url: "/api/allWines/age",
+    }).then((response) => {
+                console.log("response", response)
+})
+})
+//         var allZipCodes = dataFromDb.map(foodpairings => foodpairings.zip);
+//         console.log('Zip Code:', allZipCodes)
+//         var chart = c3.generate({
+//             bindto: '#chart',
+//             data: {
+//                 columns: [
+//                     ["Count", count],
+//                     ["Zip", allZipCodes],
+//                 ],
+//                 type: 'bar',
+//                 //type: 'donut',
+//                 // title: 'Zip Code',
+
+//                 onmouseover: function (d, i) {
+//                     console.log("onmouseover", d, i, this);
+//                 },
+//                 onmouseout: function (d, i) {
+//                     console.log("onmouseout", d, i, this);
+//                 },
+//                 onclick: function (d, i) {
+//                     console.log("onclick", d, i, this);
+//                 },
+//             },
+//             axis: {
+//                 x: {
+//                     label: 'Zip Code'
+//                 },
+//                 y: {
+//                     label: 'Count'
+//                 }
+//             }
+
+//         });
+//         // setTimeout(function () {
+//         //     chart.load({
+//         //         columns: [
+//         //             ["wine", 130],
+//         //         ]
+//         //     });
+//         // }, 1000);
+
+//         // setTimeout(function () {
+//         //     chart.unload({
+//         //         ids: 'Merlot'
+//         //     });
+//         // }, 2000);    
+//     });
+// });
 
 
 //     newFunction();
@@ -67,4 +69,3 @@ $(document).ready(function () {
 //         console.log('Zip', zip);
 //     });
 // }
-    
