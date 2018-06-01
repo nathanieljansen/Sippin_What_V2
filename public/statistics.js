c6f59f7c8b4c75217bcd2bfb82ade230ed74071f
Nathaniel Jansen <nathanieljansen@gmail.com>
2018-05-31 18:14:58 -0600

    graphs added

--- public/statistics.js
+++ public/statistics.js
@@ -6,40 +6,128 @@ $(document).ready(function () {
         method: "GET",
         url: "/api/allWines/age",
     }).then((response) => {
-        const chartObject = {
-                data: {
-                    xs: {
-                        age: "age_x"
-                    },
-                    columns: [
-                        ["age", ...response.map(age => age.ageCount)],
-                        ["age_x", ...response.map(age => age.age)]
-                        
-                    ],
-                    type: "scatter"
+        const ageChart = c3.generate({
+            bindto: ".ageChart",
+            data: {
+                xs: {
+                    age: "age_x"
                 },
-                axis: {
-                    x: {
-                        label: "Age",
-                        tick: {
-                            fit: false
-                        }
-                    },
-                    y: {
-                        label: "Count"
+                columns: [
+                    ["age", ...response.map(age => age.ageCount)],
+                    ["age_x", ...response.map(age => age.age)]
+
+                ],
+                type: "bar"
+            },
+            axis: {
+                x: {
+                   type: "category",
+                    label: "Age",
+                    tick: {
+                        fit: false
                     }
+                },
+                y: {
+                    label: "Count"
                 }
             }
-
-        
-
-        const ageChart = c3.generate({ ...chartObject})
-
-        $("#chart").append(ageChart)
+        })
     })
-    
+    $.ajax({
+        method: "GET",
+        url: "/api/allWines/zip",
+    }).then((response) => {
+        const zipChart = c3.generate({
+            bindto: ".zipChart",
+            data: {
+                xs: {
+                    zip: "zip_x"
+                },
+                columns: [
+                    ["zip", ...response.map(zip => zip.zipCount)],
+                    ["zip_x", ...response.map(zip => zip.zip)]
+
+                ],
+                type: "bar"
+            },
+            axis: {
+                x: {
+                    type: 'category',
+                    label: "Zip",
+                    tick: {
+                        fit: false
+                    }
+                },
+                y: {
+                    label: "Count"
+                }
+            }
+        })
+    })
+
+   $.ajax({
+       method: "GET",
+       url: "/api/allWines/firstmatch",
+   }).then((response) => {
+       const wineChart = c3.generate({
+           bindto: ".wineChart",
+           data: {
+               xs: {
+                   first_match: "first_match_x"
+               },
+               columns: [
+                   ["first_match", ...response.map(first_match => first_match.first_matchCount)],
+                   ["first_match_x", ...response.map(first_match => first_match.first_match)]
+
+               ],
+               type: "bar"
+           },
+           axis: {
+               x: {
+                   type: 'category',
+                   label: "Wine",
+                   tick: {
+                       fit: false
+                   }
+               },
+               y: {
+                   label: "Count"
+               }
+           }
+       })
+   })
+
+    $.ajax({
+        method: "GET",
+        url: "/api/allWines/food",
+    }).then((response) => {
+        const wineChart = c3.generate({
+            bindto: ".foodChart",
+            data: {
+                xs: {
+                    food: "food_x"
+                },
+                columns: [
+                    ["food", ...response.map(food => food.foodCount)],
+                    ["food_x", ...response.map(food => food.food)]
+
+                ],
+                type: "bar"
+            },
+            axis: {
+                x: {
+                    type: 'category',
+                    label: "Food",
+                    tick: {
+                        fit: false
+                    }
+                },
+                y: {
+                    label: "Count"
+                }
+            }
+        })
+    })
 })
-
-
 
 
