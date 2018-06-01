175a81821580abc98a2f6906aa06979b106189ac
Nathaniel Jansen <nathanieljansen@gmail.com>
2018-06-01 17:48:35 -0600

    empty zips corrected

--- server.js
+++ server.js
@@ -141,7 +141,7 @@ app.get("/api/ages", (req, res) => {
 });
 
 
-db.sequelize.sync().then(() => {
+db.sequelize.sync({force:true}).then(() => {
   app.listen(PORT, () => {
     console.log("App listening on PORT " + PORT);
   });
