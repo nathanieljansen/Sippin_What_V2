2867887db9a2cfc2feb0fd2dbea5c0cc36f065bb
Nathaniel Jansen <nathanieljansen@gmail.com>
2018-06-01 19:31:22 -0600

    idk

--- server.js
+++ server.js
@@ -142,7 +142,7 @@ app.get("/api/ages", (req, res) => {
 });
 
 
-db.sequelize.sync({force:true}).then(() => {
+db.sequelize.sync().then(() => {
   app.listen(PORT, () => {
     console.log("App listening on PORT " + PORT);
   });
